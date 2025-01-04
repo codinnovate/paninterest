b71bc0118524e11a15da129cf8cb7eb19a10e1e9
samuel <adeyemis710@gmail.com>
2025-01-04 07:48:12 +0100

    added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import LoadMore from '@/components/LoadMore';
 import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
 import { fetchAnime } from './action';
+import Header from '@/components/Header';
 
 export default async function Home() {
   const data = await fetchAnime(1, 'popularity', 5);
   return (
    <main>
-    <h2>Explore Anime</h2>
-    <section className='flex flex-col items-center sm:grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10'>
+    <Header />
+    <section className='mt-5 p-2 flex flex-col items-center sm:grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10'>
       {data.map((item:AnimeProp, idx:number) => (
         <AnimeCard 
         key={idx} 
